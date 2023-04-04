7405291fd64eca968da7204927d5018d5fa7cd35
hahaws666 <109090362+hahaws666@users.noreply.github.com>
2023-04-03 20:28:16 -0400

    Add files via upload

--- 20A3/app.py
+++ 20A3/app.py
@@ -4,7 +4,19 @@ app = Flask(__name__,template_folder="template")
 
 @app.route('/')
 def home():
-    return render_template("donating.html")
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/gallary')
+def gallary():
+    return render_template("gallary.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 
 app.run()
